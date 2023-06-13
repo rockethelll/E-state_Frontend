66763005f568efc78b1b933c152a4f0db6e6a10a
rockethelll <rockethell@hotmail.com>
2023-06-13 10:42:28 +0200

    test fecth api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
 
 const home = () => {
+  
+  const fetchApi = () => {
+    try {
+      const response = fetch('https://estate-api.herokuapp.com/estates')
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+
+    } catch (error){
+    console.log(error.message)
+    }
+  } 
+  fetchApi()
+
   return (
     <div className="text-3xl font-bold underline">
       <h1>Home page</h1>
+
     </div>
   );
 };
